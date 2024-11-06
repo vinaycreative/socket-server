d35e41c44e546c409639f9c601184eb6f90a9c5c
vinaycreative <majorcoderr@gmail.com>
2024-11-06 17:46:00 +0530

    fix: port bug resolve

--- server.js
+++ server.js
@@ -124,6 +124,8 @@ io.on("connection", (socket) => {
   })
 })
 
+const PORT = process.env.PORT || 3000
+
 // Start the server
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
